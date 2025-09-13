c75b3b41be755facd52003d41973b7bec2715980
aB-temps <187414947+aB-temps@users.noreply.github.com>
2025-09-13 11:58:04 +0200

    ✨ feat(kitty/fish): dotfiles

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,9 @@
+#=========== APP
+alias n="nvim"
+
+#=========== CODE
+alias comp="cc -Wall -Werror -Wextra"
+alias vlg="valgrind --leak-check=full --trace-children=yes --track-fds=yes"
+
+#=========== SCRIPTS
 alias cmt="/sgoinfre/abetemps/Public/commit_maker.sh"
