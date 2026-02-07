50db0cca7e86580473f8b8cc634288e65982052e
leonardecavele <leonard.decavele@gmail.com>
2026-02-07 13:35:06 +0000

    edit: changed junest repository and added username variable

--- scripts/junest.sh
+++ scripts/junest.sh
@@ -3,12 +3,12 @@
 # strict with errors
 set -euo pipefail
 
-# get library
-source "$SCRIPT_DIRECTORY/scripts/lib.sh"
+# get logger
+source "$SCRIPT_DIRECTORY/scripts/log.sh"
 
 # clone junest repository
 if [ ! -d "$JUNEST_REPOSITORY" ]; then
-  git clone https://github.com/fsquillace/junest.git "$JUNEST_REPOSITORY"
+  git clone https://github.com/leonardecavele/junest.git "$JUNEST_REPOSITORY"
 else
   log_info "junest repository already exists: ${JUNEST_REPOSITORY}"
   exit 0
@@ -19,4 +19,7 @@ log_info "installing junest"
 "$JUNEST" setup
 "$JUNEST" -f -- sudo pacman --noconfirm -Syy
 "$JUNEST" -f -- sudo pacman --noconfirm -Sy archlinux-keyring
+uid="$(id -u)"
+grep -qE "^[^:]+:x:${uid}:" "$HOME/etc/passwd" || \
+echo "${user}:x:${uid}:${uid}:${user}:/home/${user}:/bin/bash" >> "$HOME/etc/passwd"
 log_info "done installing junest"
