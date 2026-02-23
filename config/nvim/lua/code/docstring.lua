1c051e57801880ae3993425d01a1b25eb07ad5d8
leonardecavele <leonard.decavele@gmail.com>
2026-02-23 16:16:11 +0100

    fix: Doge

--- config/nvim/lua/code/docstring.lua
+++ config/nvim/lua/code/docstring.lua
@@ -1,10 +1,15 @@
 -- https://github.com/kkoomen/vim-doge
-if vim.fn.exists(':DogeGenerate') then
-	vim.api.nvim_create_user_command('DogeAll', function()
-	  vim.cmd([[g/^\s*\(def\s\+\k\+\s*(\|class\s\+\k\+\)/DogeGenerate google]])
-	  vim.cmd([[ %s/\("""\)\[TODO:summary\]\n\(\s*\)/\1\2/g ]])
-	  vim.cmd([[ %s/\[TODO:description\]/\[TODO:brief_description\]/g ]])
-	end, {})
 
-	keymap('n', ' x', '<Plug>(doge-generate)', { desc = "Generate Docstring for current cursor position" })
-end
+vim.api.nvim_create_user_command('DogeAll', function()
+  if vim.fn.exists(':DogeGenerate') ~= 2 then
+    vim.notify('vim-doge n’est pas chargé (commande :DogeGenerate introuvable)')
+    return
+  end
+
+  vim.cmd([[silent! g/^\s*\(def\s\+\k\+\s*(\|class\s\+\k\+\)/DogeGenerate numpy]])
+
+  vim.cmd([[silent! %s/\("""\)\[TODO:summary\]\n\(\s*\)/\1\2/ge]])
+  vim.cmd([[silent! %s/\[TODO:description\]/[TODO:brief_description]/ge]])
+end, {})
+
+vim.keymap.set('n', ' x', '<Plug>(doge-generate)', { desc = 'Generate Docstring for current cursor position' })
